c47167e2b7175a510ee3292503d4db098a5b0110
caioalonso827 <caioca827@gmail.com>
2026-09-02 11:07:13 -0300

    Erros bobos

--- locacaoEquipamentos/models/Movimentacao.cs
+++ locacaoEquipamentos/models/Movimentacao.cs
@@ -12,16 +12,27 @@ namespace locacaoEquipamentos.models
 
         [Key]
         public int id { get; set; }
+
+        [Column("usuario_id")]
         public int usuario_id { get; set; }
+
+        [Column("equipamento_id")]
         public int equipamento_id { get; set; }
 
+
+        [Column("tipo")]
         public Tipo tipo { get; set; }
 
+        [Column("quantidade")]
         public int quantidade { get; set; }
 
+
+        [Column("usuario")]
         [JsonIgnore]
         public usuario usuario { get; set; }
 
+
+        [Column("equipamento")]
         [JsonIgnore]
         public equipamentos equipamento { get; set; }
 
--- locacaoEquipamentos/models/Usuario.cs
+++ locacaoEquipamentos/models/Usuario.cs
@@ -8,9 +8,16 @@ namespace locacaoEquipamentos.models
     public class usuario
     {
         [Key]
+        [Column("id")]
         public int id {  get; set; }
+
+        [Column("nome")]
         public string nome { get; set; }
+
+        [Column("email")]
         public string email { get; set; }
+
+        [Column("senha")]
         public string senha { get; set; }
     }
 }
